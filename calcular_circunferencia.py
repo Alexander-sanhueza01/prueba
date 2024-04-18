b66dfd659facbe1318733ada6d6eb1517a52c8fc
Alexander-sanhueza01 <alexander.sanhueza.3e@gmail.com>
2024-04-18 20:14:10 +0000

    hola

---
+++ calcular_circunferencia.py
@@ -0,0 +1,10 @@
+import math
+pi = 3.141592
+radio = float(input("ingrese el radio: "))
+def calcular_circunferencia(radio):
+        return 2 * pi * radio
+
+circunferencia= calcular_circunferencia(radio)
+print("La circunferencia es:",(circunferencia))
+
+
